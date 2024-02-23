dec99ceb0a3fb7c0da5338038989c33fdc08365f
Anna Palamar <anna.palamar1210@gmail.com>
2024-02-23 22:43:13 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,151 @@
+import sys
+from pathlib import Path
+import shutil
+import re
+from threading import Thread, RLock
+
+CATEGORIES = {"Audio": [".mp3", ".wav", ".flac", ".wma"],
+              "Docs": [".docx", ".txt", ".pdf"],
+              "Images": ['.jpeg', '.png', '.jpg', '.svg'],
+              "Archives": ['.zip', '.gz', '.tzr'],
+              "Video": ['.avi', '.mp4', '.mov', '.mkv'],
+              "Unknown": []
+              }
+
+known_list = set()
+unknown_list = set()
+
+CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+               "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
+
+TRANS = {}
+for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+    TRANS[ord(c)] = l
+    TRANS[ord(c.upper())] = l.upper()
+
+
+def normalize(file_name):
+    file_name_root = file_name.stem
+    suffix = file_name.suffix
+    changed_file_name = file_name_root
+    changed_file_name = re.sub(r"\W", "_", file_name_root)
+    transliterated_name = ''.join(
+        [TRANS.get(ord(c), c) for c in changed_file_name])
+    return transliterated_name+suffix
+
+
+def check_parent_empty(parent: Path):
+    if not any(parent.iterdir()):
+        parent.rmdir()
+        # Ensure it doesn't go beyond the root directory
+        if parent != Path(sys.argv[1]):
+            check_parent_empty(parent.parent)
+
+
+def get_categories(file: Path) -> str:
+    ext = file.suffix.lower()
+
+    category_found = None
+    for cat, exts in CATEGORIES.items():
+        if ext in exts:
+            known_list.add(ext)
+            category_found = cat
+            break
+    if not category_found:
+        unknown_list.add(ext)
+
+    return category_found or "Unknown"
+
+
+def create_file_category(file: Path, category: str, dir: Path) -> None:
+    target_dir = dir.joinpath(category)
+    # Create dir if doesn't exist
+    if not target_dir.exists():
+        target_dir.mkdir()
+
+
+def rename_and_move_file(file: Path, category: str, dir: Path) -> None:
+    target_dir = dir.joinpath(category)
+    if category == "Unknown":
+        new_path = target_dir / file.name
+    else:
+        normalized_name = normalize(file)
+        new_path = target_dir / normalized_name
+    counter = 1
+    extension = file.suffix
+    while new_path.exists():
+        base_name = file.stem
+        new_name = f"{base_name}_{counter}{extension}"
+        new_path = target_dir / new_name
+        counter += 1
+    # Rename (or move) the file to the target directory
+    file.rename(new_path)
+
+
+def archive_unpack(file: Path, dir: Path):
+    try:
+        shutil.unpack_archive(str(file), dir.joinpath(file.stem))
+    except shutil.ReadError:
+        print(f"Unable to unpack {file}. It may not be a valid archive.")
+
+
+# Reentrant lock for synchronization
+lock = RLock()
+
+
+def sort_folder(path: Path) -> None:
+    known_extensions = set()
+    unknown_extensions = set()
+    elements = list(path.rglob("*"))  # get elements of the folder
+
+    # Move&rename files to appropriate categories
+    for element in elements:
+        if element.is_file():
+            category = get_categories(element)
+            create_file_category(element, category, path)
+            if category == "Archives":
+                archive_unpack(element, path.joinpath("Archives"))
+            rename_and_move_file(element, category, path)
+
+    # Update known_extensions and unknown_extensions
+            if category != "Unknown":
+                with lock:
+                    known_extensions.add(element.suffix.lower())
+            else:
+                with lock:
+                    unknown_extensions.add(element.suffix.lower())
+
+   # Update global known_list and unknown_list after processing
+    with lock:
+        known_list.update(known_extensions)
+        unknown_list.update(unknown_extensions)
+
+    # Remove dirs if they are empty
+    for element in elements:
+        if element.is_dir() and not any(element.iterdir()):
+            element.rmdir()
+            check_parent_empty(element.parent)
+
+
+def main() -> str:
+    try:
+        path = Path(sys.argv[1])
+    except IndexError:
+        return "No path to folder"
+
+    if not path.exists():
+        return "Folder does not exist"
+
+    thread = Thread(target=sort_folder, args=(path,))
+    thread.start()
+    thread.join()
+
+    print(f"Known extensions: {known_list}")
+    print(f"Unknown extensions: {unknown_list}")
+
+    return "All Ok"
+
+
+if __name__ == '__main__':
+    main()
